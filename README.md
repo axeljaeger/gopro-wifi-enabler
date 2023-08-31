8c7c6a6a9c7de7674702263ba9f26da0056861b3
Axel Jäger <axeljaeger@gmail.com>
2023-09-01 00:54:57 +0200

    Use MAUI to implement views

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # GoPro WiFI Enabler
-
+*Enabler to work with the GoPro API*
 [![Static Badge](https://img.shields.io/badge/Go%20to%20app-black?logo=github)](https://axeljaeger.github.io/goprowifienabler/)
 
-
-*Enabler to work with the GoPro API*
-
-In order to start developing applications against the GoPro WiFI API, you need to enable the WIFI interface using Bluetooth. This application gives you a fast and easy to do this, so you can start working with the WIFI API. The technical foundation is the [WebHID API](https://developer.mozilla.org/en-US/docs/Web/API/WebHID_API).
+In order to start developing applications against the GoPro WiFI API, you need to enable the WIFI interface using Bluetooth. This application gives you a fast and easy to do this, so you can start working with the WIFI API. The technical foundation is the [WebUSB API](https://developer.mozilla.org/en-US/docs/Web/API/WebUSB_API).
 
 ## Plattform Support ##
 This application was successfully tested on Mac using Chrome. It will not work in Safari or Firefox due to the missing WebHID API. Chrome on Windows was so far not successfull.
--- src/Greeter.tsx
+++ src/Greeter.tsx
@@ -1,15 +1,36 @@
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import IconButton from '@mui/material/IconButton';
+import Link from '@mui/material/Link';
+
+
+import { SiGithub } from '@icons-pack/react-simple-icons';
+
 interface Props {
     onConnect: () => void;
 }
 
 export default function Greeter({ onConnect } : Props) {
     return (
-        <>
-        <h1>GoPro WiFi Enabler</h1>
-        <div>Not an offering from GoPro</div>
-        <p><a href="https://gopro.github.io/OpenGoPro/ble_2_0">GoPro BLE API</a></p>
-        <a href="https://axeljaeger.github.io/goprowifienabler/"><img alt="GitHub" src="github-mark.svg" width={24}></img></a>
-        <button onClick={onConnect}>Connect to camera</button>
-        </>
+<Card>
+<CardHeader
+        action={
+          <IconButton aria-label="settings" href="https://axeljaeger.github.io/goprowifienabler/">
+            <SiGithub />
+          </IconButton>
+        }
+        title="GoPro WiFi Enabler"
+        subheader="Not an offering from GoPro"
+      />
+<CardContent>
+    <p><Link href="https://gopro.github.io/OpenGoPro/ble_2_0">GoPro BLE API</Link></p>
+</CardContent>
+<CardActions>
+    <Button variant="contained" onClick={onConnect} style={{margin: 'auto'}}>Connect to camera</Button>
+</CardActions>
+</Card>
     )
 }
