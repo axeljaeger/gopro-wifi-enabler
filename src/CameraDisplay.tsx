3ec3d3e318f4ced003b285153c2345743364fc07
Axel Jäger <axeljaeger@gmail.com>
2023-09-02 23:12:23 +0200

    Use MUI for everything, put QR Code in expandable.

--- src/CameraDisplay.tsx
+++ src/CameraDisplay.tsx
@@ -8,8 +8,30 @@ import TextField from "@mui/material/TextField/TextField";
 import CardActions from "@mui/material/CardActions/CardActions";
 import Button from "@mui/material/Button/Button";
 import InputAdornment from "@mui/material/InputAdornment/InputAdornment";
-import IconButton from "@mui/material/IconButton/IconButton";
+import IconButton, {
+  IconButtonProps,
+} from "@mui/material/IconButton/IconButton";
 import { ContentCopy } from "@mui/icons-material";
+import { styled } from "@mui/material/styles";
+import React from "react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Collapse } from "@mui/material";
+import QRCode from "react-qr-code";
+
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 interface Props {
   wifiApActive: boolean;
@@ -26,6 +48,12 @@ const CameraDisplay = ({
   onWifiEnabledChange,
   onDisconnect,
 }: Props) => {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <Card sx={{ minWidth: 275, maxWidth: 300 }}>
       <CardContent>
@@ -50,15 +78,14 @@ const CameraDisplay = ({
             readOnly: true,
             endAdornment: (
               <InputAdornment position="end">
-                <IconButton
-                  edge="end"
-                >
+                <IconButton edge="end">
                   <ContentCopy />
                 </IconButton>
               </InputAdornment>
-            )
+            ),
           }}
-          margin="dense" fullWidth
+          margin="dense"
+          fullWidth
         />
         <TextField
           label="Password"
@@ -68,24 +95,37 @@ const CameraDisplay = ({
             readOnly: true,
             endAdornment: (
               <InputAdornment position="end">
-                <IconButton
-                  edge="end"
-                >
+                <IconButton edge="end">
                   <ContentCopy />
                 </IconButton>
               </InputAdornment>
-            )
+            ),
           }}
-          margin="dense" fullWidth
+          margin="dense"
+          fullWidth
         />
         <WifiJoin wifiAp={wifiAp} wifiPw={wifiPw} />
       </CardContent>
 
-      <CardActions>
-        <Button type="button" onClick={onDisconnect} variant="contained" style={{margin: 'auto'}}>
+      <CardActions disableSpacing>
+        <Button type="button" onClick={onDisconnect} variant="outlined">
           Disconnect
         </Button>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
       </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <QRCode
+          value={`WIFI:T:WPA;S:${wifiAp};P:${wifiPw};;`}
+          style={{ margin: "20px" }}
+        />
+      </Collapse>
     </Card>
   );
 };
