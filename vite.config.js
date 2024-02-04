6df9f817ff0329183b05054811b0e63b5515bae5
Axel Jäger <axeljaeger@gmail.com>
2024-02-04 13:45:09 +0100

    Fix building using vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
-  return {
+export default defineConfig(() => ({
+    base: "https://axeljaeger.github.io/gopro-wifienabler/",
     build: {
       outDir: 'build',
     },
     plugins: [react()],
-  };
-});
+  })
+);
